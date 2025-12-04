9b4dbc3ebc81c15d9935a79cabb3804223bca9a7
Pablo <pablomelofrancafilho11@gmail.com>
2025-12-04 11:26:09 -0300

    Component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,18 +1,33 @@
 package com.educandoweb.course.resources;
 
-import com.educandoweb.course.entities.User;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
     @GetMapping
-    private ResponseEntity<User> findAll() {
-        User user = new User();
-        return ResponseEntity.ok().body(user);
+    private ResponseEntity<List<User>> findAll() {
+      List <User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
+    }
+    
+    @GetMapping(value = "/{id}")
+    private ResponseEntity<User> findById (@PathVariable Long id) {
+    	User obj = userService.findById(id);
+    	return ResponseEntity.ok().body(obj);
     }
  }
