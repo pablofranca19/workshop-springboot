260f444fcae8d3da7ea9d13bf0ab34f8eb7df005
Pablo <pablomelofrancafilho11@gmail.com>
2025-12-05 20:59:14 -0300

    Relationing class User with class Order

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,9 +1,16 @@
 package com.educandoweb.course.entities;
 
-import jakarta.persistence.*;
-
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +23,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    
+    @OneToMany(mappedBy = "client")
+    private List <Order> orders = new ArrayList<>();
+    
 
     public User() {}
 
@@ -66,8 +77,12 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    public List<Order> getOrders() {
+		return orders;
+	}
 
-    @Override
+	@Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
